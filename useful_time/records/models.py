f8fba9e661e421093da1b78ba1654a2e85c48bcb
Mihendy <kryukovm1khael@yandex.ru>
2022-05-05 20:51:43 +0500

    Models: migrations, some methods, simplified work with time

--- useful_time/records/models.py
+++ useful_time/records/models.py
@@ -18,12 +18,32 @@ class Record(models.Model):
     def __str__(self):
         return self.name
 
-    def get_back_longitude(self):
-        pass
+    def get_back_longitude(self) -> int:
+        """Метод для бэка. Возвращает время, потраченное на задание в секундах.
+         Если запись ещё не завершена, то вернет -1"""
+        if self.endpoint is None:
+            return -1
+        time = self.endpoint - self.startpoint
+        return time.seconds + (time.days * 24 * 60 * 60)
 
-    def get_front_longitude(self):
-        pass
-
-
-
-
+    def get_front_longitude(self) -> str:
+        """Метод для фронта. Возвращает время, потраченное на задание в человекочитаемом формате.
+         Если запись ещё не завершена, то вернет строку 'Ещё идет' """
+        if self.endpoint is None:
+            return 'Ещё идет'
+        out = []
+        datetime = (self.endpoint - self.startpoint)
+        time = datetime.seconds
+        seconds = time % 60
+        minutes = time // 60 % 60
+        hours = time // 60 // 60 % 24
+        days = datetime.days
+        if days:
+            out.append(f'{days} дн')
+        if hours:
+            out.append(f'{hours} ч')
+        if minutes:
+            out.append(f'{minutes} мин')
+        if seconds:
+            out.append(f'{seconds} сек')
+        return ' '.join(out)
