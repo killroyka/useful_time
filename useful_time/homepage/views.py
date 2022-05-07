dbcbb6430e0d5c2d6b7a0e468d788f03bbcf0504
killroyka <killerbeesexy@gmail.com>
2022-05-07 09:19:41 +0300

    homepage.html addet

--- useful_time/homepage/views.py
+++ useful_time/homepage/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.views import View
 
 class HomePage(View):
-    template_name = "base.html"
+    template_name = "homepage/homepage.html"
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
