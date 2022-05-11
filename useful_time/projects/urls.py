ee2ec59275d6d66f8c3858678fca12ad8f6dc07a
Mihendy <kryukovm1khael@yandex.ru>
2022-05-11 17:32:34 +0500

    project and record_list separation

--- useful_time/projects/urls.py
+++ useful_time/projects/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import ProjectsListView, ProjectAddView
-from records.views import RecordListView
+from .views import ProjectsListView, ProjectAddView, ProjectView
 
 
 urlpatterns = [
     path("", ProjectsListView.as_view(), name="projects_list"),
     path("add/", ProjectAddView.as_view(), name="project_add"),
-    path('<int:id>/', RecordListView.as_view(), name="project_records_list"),
+    path('<int:id>/', ProjectView.as_view(), name="project_records_list"),
 ]
--- useful_time/projects/views.py
+++ useful_time/projects/views.py
@@ -1,6 +1,14 @@
+from datetime import datetime
+
+from django.db.models import Prefetch
+from django.urls import reverse_lazy
+from django.views.generic import ListView
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+from records.models import Record
+from records.views import RecordListView
 from .models import Project
 from .forms import NewProjectForm
 from django.shortcuts import redirect
@@ -35,3 +43,30 @@ class ProjectAddView(LoginRequiredMixin, FormView):
             project.save()
             return redirect('/projects')
         return redirect('/projects/add')
+
+
+class ProjectView(LoginRequiredMixin, ListView):
+    model = Record
+    template_name = 'projects/project.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        id_ = self.kwargs.get('id')
+
+        context = super(ProjectView, self).get_context_data(**kwargs)
+        project = Project.objects.filter(user_id=self.request.user.id, id=id_)
+        prefetch_projects = Prefetch('project', queryset=project)
+        context['records'] = Record.objects.filter(project_id=id_).prefetch_related(prefetch_projects)
+        context['title'] = project.first().name
+        return context
+
+    def post(self, request, **kwargs):
+        project_id = kwargs.get('id')
+        if 'project_delete' in request.POST:
+            project = Project.objects.get(id=project_id)
+            project.delete()
+            return redirect(f'/projects/')
+        record_id = int(request.POST.get('id'))
+        record = Record.objects.get(pk=record_id)
+        record.endpoint = datetime.now()
+        record.save()
+        return redirect(f'/projects/{project_id}/')
