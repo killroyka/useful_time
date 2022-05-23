bb4ec7376cc4be9cbe3e70e897f3f3d3f99edeb5
killroyka <killerbeesexy@gmail.com>
2022-05-23 18:47:42 +0300

    timezone bug fix

--- useful_time/static_dev/js/timer.js
+++ useful_time/static_dev/js/timer.js
@@ -2,19 +2,27 @@ var days, hours, minutes, seconds;
 
 all_counters = document.querySelectorAll('.counter');
 all_stop_counters = document.querySelectorAll('.counter_stop');
+all_timepoint = document.querySelectorAll(".timepoint")
+var diff_UTC_time = new Date().getTimezoneOffset() * 60
+console.log(all_timepoint)
 
-for (element of all_stop_counters) {
+Array.from(all_timepoint).forEach((element, index) => {
+    console.log(new Date(element.dataset.time).getTime())
+    var time_with_timezone = new Date().setTime(new Date(element.dataset.time).valueOf() + (diff_UTC_time * 1000));
+    console.log(time_with_timezone)
+    element.innerHTML = String(new Date().setTime(time_with_timezone).toString());
+});
+console.log(all_timepoint)
+Array.from(all_stop_counters).forEach((element, index) => {
     element.dataset.time = String(Number(element.dataset.time) + 1);
     element.innerHTML = getTime(element.dataset.time);
     localStorage.setItem(element.dataset.id, "0")
-}
+});
 Array.from(all_counters).forEach((element, index) => {
-    console.log(element.dataset.time - Number(localStorage.getItem(element.dataset.id)))
     element.dataset.time = String(Number(element.dataset.time) +
         Number(localStorage.getItem(element.dataset.id)))
     element.innerHTML = getTime(element.dataset.time);
-})
-;
+});
 
 setInterval(function () {
     Array.from(all_counters).forEach((element, index) => {
@@ -53,5 +61,4 @@ function getTime(start_date) {
         ans += ' <span class="seconds">' + seconds + ' сек</span>'
     }
     return ans;
-
 };
